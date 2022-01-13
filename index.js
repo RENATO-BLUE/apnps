6cda41f7a9d411631c5dd054297ed8abd88cec5d
RENATO SANTOS PEREIRA <engenheirorenatoes@gmail.com>
2022-01-13 16:42:21 -0300

    teste

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var path = require('path')
+
+app.use(cookieParser())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.set("view engine","ejs")
+
+app.use(express.static(path.join(__dirname,"public")))
 
 app.get('/', function(req, res){
     res.send("Olá Renato!")
 })
-
-app.get('/msg', function(req,res){
-    res.send("Essa mensagem é automática!")
-})
-
-app.get('/Sobre', function(req,res){
-    res.send("Essa página esta sendo desenvolvido pelo Renato!")
-})
-
-app.get('/layout', function(req,res){
-    res.send("Esse é o novo layout!")
-})
-
 
 app.listen(3333, function(){
     console.log("Conexão iniciada...")
