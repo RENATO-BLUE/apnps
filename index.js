6923662e66dffaf717a29e81e5dc991b53960a2c
RENATO SANTOS PEREIRA <engenheirorenatoes@gmail.com>
2022-01-13 15:28:08 -0300

    Primeira versão do projeto

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function(req, res){
+    res.send("Olá Mundo!")
+})
+
+app.listen(3333, function(){
+    console.log("Conexão inicializada na porta 3333")
+})
